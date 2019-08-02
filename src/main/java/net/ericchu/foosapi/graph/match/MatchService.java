b57c237f009e62b02039028c88a85d5cfc3bb87b
Eric Chu <eric@ericchu.net>
2019-08-02 22:16:07 +0800

    Add get match

--- src/main/java/net/ericchu/foosapi/graph/match/MatchService.java
+++ src/main/java/net/ericchu/foosapi/graph/match/MatchService.java
@@ -1,5 +1,6 @@
 package net.ericchu.foosapi.graph.match;
 
+import com.google.common.base.Optional;
 import com.google.common.util.concurrent.FutureCallback;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Mono;
@@ -19,6 +20,23 @@ public class MatchService {
             @Override
             public void onSuccess(List<Match> result) {
                 sink.success(result);
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                sink.error(t);
+            }
+        }));
+    }
+
+    public Publisher<Match> getMatch(String id) {
+        return Mono.create(sink -> matchRepository.findById(id).fetchFirst().addCallback(new FutureCallback<>() {
+            @Override
+            public void onSuccess(Optional<Match> result) {
+                if (result.isPresent())
+                    sink.success(result.get());
+                else
+                    sink.success();
             }
 
             @Override
